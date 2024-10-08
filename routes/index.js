602163c8eb7aeb3b31880234448ef7adb8f2af32
Preslav977 <p.cvetanow@gmail.com>
2024-10-08 10:58:06 +0300

    Created new router in index and array of messages.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,25 @@ const express = require("express");
 
 const router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 router.get("/", (req, res, next) => {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Mini Message Board", messages: messages });
+});
+
+router.get("/new", (req, res, next) => {
+  res.render("");
 });
 
 module.exports = router;
