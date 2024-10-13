0b3e3525a9b6a098364f47db8f752b52488c223e
Preslav977 <p.cvetanow@gmail.com>
2024-10-13 11:51:15 +0300

    Bringed back the env variables to see if it is going to work.

--- db/pool.js
+++ db/pool.js
@@ -2,6 +2,8 @@ const { Pool } = require("pg");
 
 const { argv } = require("node:process");
 
+require("dotenv").config();
+
 module.exports = new Pool({
-  connectionString: `${argv[2]}`,
+  connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost:5432/${process.env.DB_NAME}`,
 });
--- db/populatedb.js
+++ db/populatedb.js
@@ -2,6 +2,8 @@ const { Client } = require("pg");
 
 const { argv } = require("node:process");
 
+require("dotenv").config();
+
 const SQL = `CREATE TABLE IF NOT EXISTS mini_msg_board (
 id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 username VARCHAR(255), message VARCHAR(255), date_added DATE);
@@ -15,7 +17,7 @@ async function main() {
   console.log("seeding...");
 
   const client = new Client({
-    connectionString: `${argv[2]}`,
+    connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost:5432/${process.env.DB_NAME}`,
   });
 
   await client.connect();
