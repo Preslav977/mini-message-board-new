43fd65ad66ceb16b367075085ae871ea6dc995af
Preslav977 <p.cvetanow@gmail.com>
2024-10-13 10:19:11 +0300

    Removed this code to see if the deployment will work.

--- db/pool.js
+++ db/pool.js
@@ -2,22 +2,6 @@ const { Pool } = require("pg");
 
 const { argv } = require("node:process");
 
-let sequelize;
-
-if (process.env.DB_URL) {
-  sequelize = new sequelize(process.env.DB_URL);
-} else {
-  sequelize = new sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-      host: "localhost",
-      dialect: "postgresql",
-    }
-  );
-}
-
 module.exports = new Pool({
   connectionString: `${argv[2]}`,
 });
