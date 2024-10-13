e9ccfd18b8e1883eb3311f6a1c5bacce1611e8bc
Preslav977 <p.cvetanow@gmail.com>
2024-10-13 11:04:23 +0300

    Removed the sequalize code and added the connection string again.

--- db/populatedb.js
+++ db/populatedb.js
@@ -11,22 +11,6 @@ INSERT INTO mini_msg_board (username, message, date_added)
 VALUES ('Preslaw', 'Hello', '11/10/2024')
 `;
 
-let sequelize;
-
-if (process.env.DB_URL) {
-  sequelize = new sequelize(process.env.DB_URL);
-} else {
-  sequelize = new sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-      host: "localhost",
-      dialect: "postgresql",
-    }
-  );
-}
-
 async function main() {
   console.log("seeding...");
 
